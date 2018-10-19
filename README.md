778e77b2068197dc069715cc8ab730de980c12c1
koofrank <koofranker@gmail.com>
2018-10-19 18:29:05 +0800

    init file

---
+++ README.md
@@ -0,0 +1,8 @@
+# cybex-ios-core-cpp
+
+Supported arch: arm64, x86_64
+
+## Installation
+You can install cybex-ios-core-cpp via Carthage by adding the following line to your Cartfile:
+
+github "CybexDex/cybex-ios-core-cpp"
---
+++ cybex-ios-core-cpp-demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  cybex-ios-core-cpp-demo
+//
+//  Created by koofrank on 2018/10/19.
+//  Copyright © 2018 com.nbltrustdev. All rights reserved.
+//
+
+import UIKit
+import cybex_ios_core_cpp
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
